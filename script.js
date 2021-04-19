067f5c93dd890b1d49f5928f23da7cade9c416a2
masampson <78632373+masampson@users.noreply.github.com>
2021-04-19 10:22:58 -0700

    Add files via upload
    This should have all the DOM selector stuff replaced with jQuery

--- script.js
+++ script.js
@@ -1,31 +1,95 @@
 "use strict";
 
-let toDoList = new Map();
-let num = 0;
-const doInput = document.getElementById("do-input");
+// JQUERY
 
-const addItem = function () {
-  if (doInput.value === "") {
-    alert("Please enter an item");
-  } else {
-    num++;
-    let item = `item${num}`;
-    //   const doInput = document.getElementById("do-input");
-    toDoList.set(item, doInput.value);
-    document.querySelector(".to-do-items").insertAdjacentHTML(
-      "beforeend",
-      `<div class="item-row" id="${item}">
-          <h3 class="item">${toDoList.get(`${item}`)}</h3>
-          <button class="delete" id="${item}-button">Did</button>
-      </div>`
+$(document).ready(function () {
+  let toDoList = [];
+
+  // CLEAR UI
+  const clearUI = () => $(".to-do-items").html("");
+  // DISPLAY ALL ITEMS
+  const displayAll = function () {
+    clearUI();
+    $.each(toDoList, function (index, item) {
+      $(".to-do-items").append(
+        `<li class="item-row">
+                  <input class="check-off" type="checkbox" name="mark-done" id="${index}"/>
+                  <label for="mark-done" class="${
+                    item.status === "active" ? "item" : "item-done"
+                  }" >${item.val}</label>
+                  <button class="delete">X</button>
+                </li>`
+      );
+    });
+  };
+
+  $("#all").click(displayAll);
+  // ADD A TO DO ITEM
+  $("#do-button").click(function (e) {
+    e.preventDefault();
+    toDoList.push({ val: $("#do-input").val(), status: "active" });
+    $("#do-input").val("");
+    displayAll();
+  });
+
+  // DISPLAY ACTIVE ITEMS
+  $("#active").click(function () {
+    clearUI();
+    $.each(
+      toDoList.filter((x) => x.status === "active"),
+      function (index, item) {
+        $(".to-do-items").append(
+          `<li class="item-row">
+                        <input class="check-off" type="checkbox" name="mark-done" id="${index}"/>
+                        <label for="mark-done" class="item" >${item.val}</label>
+                        <button class="delete">X</button>
+                      </li>`
+        );
+      }
     );
-    document
-      .getElementById(`${item}-button`)
-      .addEventListener("click", function () {
-        document.getElementById(`${item}`).remove();
-      });
-    doInput.value = "";
-  }
-};
+  });
+  // DISPLAY COMPLETED ITEMS
+  $("#done").click(function () {
+    clearUI();
+    $.each(
+      toDoList.filter((x) => x.status === "complete"),
+      function (index, item) {
+        $(".to-do-items").append(
+          `<li class="item-row">
+                        <input class="check-off" type="checkbox" name="mark-done" id="${index}"/>
+                        <label for="mark-done" class="item-done" >${item.val}</label>
+                        <button class="delete">X</button>
+                      </li>`
+        );
+      }
+    );
+  });
+  // DELETE OR MARK COMPLETED
+  $(".to-do-items").click(function (e) {
+    const item = e.target;
+    const itemClass = item.classList[0];
+    if (itemClass === "delete") {
+      toDoList.splice(item.previousElementSibling.previousElementSibling.id, 1);
+      displayAll();
+    }
+    if (itemClass === "check-off") {
+      if (toDoList[item.id].status === "complete") {
+        toDoList[item.id].status = "active";
+      } else if (toDoList[item.id].status === "active") {
+        toDoList[item.id].status = "complete";
+      }
+      displayAll();
+    }
+  });
 
-document.getElementById("do-button").addEventListener("click", addItem);
+  // KILL ALL COMPLETED ITEMS
+  $("#kill").click(function () {
+    for (let i = toDoList.length - 1; i >= 0; i--) {
+      if (toDoList[i].status === "complete") {
+        toDoList.splice(i, 1);
+      }
+    }
+    displayAll();
+  });
+  // END OF JQUERY
+});
